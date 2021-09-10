3e1419c88912260e03a956439e611c79fc1f011a
SCOUT\l.mardanyan <l.mardanyan@scout-gps.ru>
2021-09-10 12:33:11 +0300

    Finished module 3

--- DeskBooker.Core.Tests/DeskBookingRequestProcessorTests.cs
+++ DeskBooker.Core.Tests/DeskBookingRequestProcessorTests.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using DeskBooker.Core.DataInterface;
 using DeskBooker.Core.Domain;
 using DeskBooker.Core.Processor;
+using Moq;
 using Xunit;
 
 namespace DeskBooker.Core
@@ -8,35 +13,52 @@ namespace DeskBooker.Core
     public class DeskBookingRequestProcessorTests
     {
         private DeskBookingRequestProcessor _processor;
+        private DeskBookingRequest _request;
+        private Mock<IDeskBookRepository> _deskBookingRepository;
+        private Mock<IDeskRepository> _deskRepository;
+        private List<Desk> _availableDesks;
+
         public DeskBookingRequestProcessorTests()
         {
-            _processor = new DeskBookingRequestProcessor();
+            _request = new DeskBookingRequest
+            {
+                FirstName = "Thomas",
+                LastName = "Huber",
+                Email = "thomas@cloudiushuber@gmail.com",
+                Date = new DateTime(2012, 12, 1)
+            };
+
+            _availableDesks = new List<Desk>
+             {
+                 new Desk{Id = 7}
+             };
+
+            _deskBookingRepository = new Mock<IDeskBookRepository>();
+            _deskRepository = new Mock<IDeskRepository>();
+
+            _deskRepository.Setup(x => x.GetAvailableDesks(_request.Date)).Returns(_availableDesks);
+
+
+            _processor = new DeskBookingRequestProcessor(_deskBookingRepository.Object, _deskRepository.Object);
 
         }
 
         [Fact]
         public void ShouldReturnDeskBookingResultWithRequestValues()
         {
-            //Arrange
-            var request = new DeskBookingRequest
-            {
-                FirstName = "Thomas",
-                LastName = "Huber",
-                Email = "thomas@cloudiushuber@gmail.com",
-                Date = new DateTime(2012,12,1)
-            };
-             
+
+
             //Act
-            DeskBookingResult result = _processor.BookDesk(request);
+            DeskBookingResult result = _processor.BookDesk(_request);
 
             //Assert
 
             Assert.NotNull(result);
 
-            Assert.Equal(request.FirstName, result.FirstName);
-            Assert.Equal(request.LastName, result.LastName);
-            Assert.Equal(request.Email, result.Email);
-            Assert.Equal(request.Date, result.Date);
+            Assert.Equal(_request.FirstName, result.FirstName);
+            Assert.Equal(_request.LastName, result.LastName);
+            Assert.Equal(_request.Email, result.Email);
+            Assert.Equal(_request.Date, result.Date);
 
         }
 
@@ -44,9 +66,81 @@ namespace DeskBooker.Core
         public void ShouldThrowExceptionIfRequestIsNull()
         {
             //Arrange
-             
+
             Assert.Throws<ArgumentNullException>(() => { _processor.BookDesk(null); });
 
+        }
+
+        [Fact]
+        public void ShouldSaveDeskBooking()
+        {
+            DeskBooking savedDeskBooking = null;
+
+            _deskBookingRepository.Setup(x => x.Save(It.IsAny<DeskBooking>()))
+                .Callback<DeskBooking>(deskBooking => { savedDeskBooking = deskBooking; });
+
+            _processor.BookDesk(_request);
+
+            _deskBookingRepository.Verify(x => x.Save(It.IsAny<DeskBooking>()), Times.Once);
+
+            Assert.NotNull(savedDeskBooking);
+
+            Assert.Equal(_request.FirstName, savedDeskBooking.FirstName);
+            Assert.Equal(_request.LastName, savedDeskBooking.LastName);
+            Assert.Equal(_request.Email, savedDeskBooking.Email);
+            Assert.Equal(_request.Date, savedDeskBooking.Date);
+            Assert.Equal(_availableDesks.First().Id, savedDeskBooking.DeskId);
+        }
+
+
+        [Fact]
+        public void ShouldNotSaveDeskBookingIfNoDeskIsAvailable()
+        {
+            _availableDesks.Clear();
+
+            _processor.BookDesk(_request);
+
+            _deskBookingRepository.Verify(x => x.Save(It.IsAny<DeskBooking>()), Times.Never);
+
+        }
+
+
+        [Theory]
+        [InlineData(DeskBookingResultCode.Success, true)]
+        [InlineData(DeskBookingResultCode.NoDeskAvailable, false)]
+        public void ShouldReturnExpectedResultCode(DeskBookingResultCode expectedResultCode, bool iSDeskAvailable)
+        {
+            if (!iSDeskAvailable)
+                _availableDesks.Clear();
+
+            var result = _processor.BookDesk(_request);
+
+            Assert.Equal(expectedResultCode, result.Code);
+        }
+
+        [Theory]
+        [InlineData(7, true)]
+        [InlineData(null, false)]
+        public void ShouldReturnExpectedDeskBookingId(int? expectedDeskBookingId, bool iSDeskAvailable)
+        {
+            if (!iSDeskAvailable)
+            {
+                _availableDesks.Clear();
+            }
+            else
+            {
+                _deskBookingRepository.Setup(x => x.Save(It.IsAny<DeskBooking>()))
+                    .Callback<DeskBooking>(deskBooking =>
+                    {
+                        deskBooking.Id = expectedDeskBookingId.Value;
+
+                    });
+            }
+
+            var result = _processor.BookDesk(_request);
+
+
+            Assert.Equal(expectedDeskBookingId, result.DeskBookingId);
         }
     }
 }
--- DeskBooker.Core/Processor/DeskBookingRequestProcessor.cs
+++ DeskBooker.Core/Processor/DeskBookingRequestProcessor.cs
@@ -1,16 +1,54 @@
 ﻿using DeskBooker.Core.Domain;
 using System;
+using System.Linq;
+using DeskBooker.Core.DataInterface;
 
 namespace DeskBooker.Core.Processor
 {
     public class DeskBookingRequestProcessor
     {
+        private readonly IDeskBookRepository _deskBookingRepository;
+        private readonly IDeskRepository _deskRepository;
+
+        public DeskBookingRequestProcessor(IDeskBookRepository deskBookingRepository,
+            IDeskRepository deskRepository)
+        {
+            _deskBookingRepository = deskBookingRepository;
+            _deskRepository = deskRepository;
+        }
+
         public DeskBookingResult BookDesk(DeskBookingRequest request)
         {
             if (request == null)
                 throw new ArgumentNullException(nameof(request));
 
-            return new DeskBookingResult
+            var result = Create<DeskBookingResult>(request);
+
+            var availableDesks = _deskRepository.GetAvailableDesks(request.Date);
+
+            if (availableDesks.FirstOrDefault() is { } availableDesk) // is Desk
+            {
+                var deskBooking = Create<DeskBooking>(request);
+
+                deskBooking.DeskId = availableDesk.Id;
+
+                _deskBookingRepository.Save(deskBooking);
+
+                result.DeskBookingId = deskBooking.Id;
+                result.Code = DeskBookingResultCode.Success;
+            }
+            else
+            {
+                result.Code = DeskBookingResultCode.NoDeskAvailable;
+            }
+
+
+            return result;
+        }
+
+        private static T Create<T>(DeskBookingRequest request) where T : DeskBookingBase, new()
+        {
+            return new T
             {
                 FirstName = request.FirstName,
                 LastName = request.LastName,
